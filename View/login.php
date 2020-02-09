5420d1f8d4cc19cf6af4a54318d06a98c344fe4d
Joshua Moore <K1720552@kingston.ac.uk>
2020-02-09 11:58:28 +0000

    Login page created.

---
+++ View/login.php
@@ -0,0 +1,122 @@
+<!doctype html>
+<?php
+include_once '../css/stylesheets.php';
+require_once '../Model/dataAccess.php';
+require_once '../Controller/login.php';
+?>
+<html lang="en">
+
+    <head>
+
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+        <link rel="stylesheet" href="../css/main.css">
+        <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+        <title></title>
+
+    </head>
+
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class='container'>
+            <div class="navbar-header">
+                <a class="navbar-brand" href="/View/index.html"><img class="navbar-brand" href="/View/index.html" src="../images/Logo.png" alt="logo" style="width:40px">| Viewsician</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </div>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <div class="nav-middle">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <input class="search_input" type="text" name="" placeholder="Search...">
+                            <a href="#" class="search_icon"><i class="fas fa-search" style="font-size:25px"></i></a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="/View/search.html" class="icon"><i class="far fa-compass" style="font-size:30px" style="padding: 20px"></i></a>
+                            <span class="text">Explore</span>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <!--<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">-->
+                            <div class="dropdown">
+                                <a href="/View/search.html" class="icon"><i class="far fa-user-circle" style="font-size:30px"></i></a>
+                                <span class="text">Account</span>
+                                <!--</a>-->
+
+                                <div class="dropdown-menu"><!--menu aria-labelledby="navbarDropdown"-->
+                                    <a class="dropdown-item" href="#">Action</a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="#">Another action</a>
+                                    <a class="dropdown-item" href="#">Something else here</a>
+                                </div>
+                            </div>
+                        </li>
+                </div>
+                <li class="nav-item">
+                    <a class="nav-link" href="/View/session.html">Session</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/View/artists.html">Artists</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/View/rehearsal.html">Rehearsal</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/View/venues.html">Venues</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/View/login.html">List</a>
+                </li>
+                </ul>
+            </div>
+        </div>
+
+    </nav>
+
+    <body>
+        <br><br>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h4>Login</h4>
+                </div>
+                <div class="col">
+
+                </div>
+                <div class="col">
+
+                </div>
+                <div class="col">
+
+                </div>
+            </div>
+            <br><br>
+            <div class="row">
+                <div class="col-md-3">
+
+                </div>
+
+                <div class="col-md-6">
+                    <div class="login-box">
+                        <form method="post">
+                            <div class="row mini-box login">Username</div>
+                            <div class="row mini-box login"><input type="text" id="login-details" name="username" placeholder="Username"></div>
+                            <div class="row mini-box login">Password</div>
+                            <div class="row mini-box login"><input type="password" id="login-details" name="password" placeholder="Password"></div>
+                            <button class="buttons" id="bbutton" type="submit" name="login" value="LOGIN">LOGIN</button>
+                        </form>
+                    </div>
+                </div>
+                <div class="col-md-3 account-button">
+                </div>
+            </div>
+
+        </div>
+        <!-- Optional JavaScript -->
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    </body>
+</html>
+
+
