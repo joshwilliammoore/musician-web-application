c4c9228d442d2e7b22a33a680ee7e6632a5076e9
Joshua Moore <K1720552@kingston.ac.uk>
2020-05-08 03:32:00 +0100

    Login page updated.

--- Controller/login.php
+++ Controller/login.php
@@ -1,49 +1,50 @@
 <?php
+
 require_once"../Model/Users.php";
 require_once"../Model/dataAccess.php";
-require_once '../View/login.php';
-
-if(!isset($_SESSION)) {session_start();}
+//require_once '../View/login.php';
+//if(!isset($_SESSION)) {session_start();}
 
 $error = false;
 
-if(isset($_SESSION["userId"]))
-{
-        header("Location: ../View/index.php");
-        exit();
-}
-else if(isset($_REQUEST["login"]))
-{
+if (isset($_SESSION["userId"])) {
+    header("Location: ../View/index.php");
+    exit();
+} else if (isset($_REQUEST["login"])) {
     $username = htmlspecialchars($_REQUEST['username']);
     $password = htmlspecialchars($_REQUEST['password']);
-    $found = getUsernameAndPassword($username,$password);
+
     $user = userLogin($username);
-    if($found != false)
-    {
-        $_SESSION['username'] = $username;
-        $_SESSION['userId'] = $found->user_id;
-        $_SESSION['userUsername'] = $user[0]->username;
-        $_SESSION['userEmail'] = $user[0]->email_address;
-        $_SESSION['userName'] = $user[0]->name;
-        $_SESSION['userPassword'] = $user[0]->password;
-        $_SESSION['profilePic'] = $user[0]->Profile_Picture;
-        
-        
-        header("Location: ../View/index.php");
+    if (sizeof($user) < 1) {
+        header("Location: ../View/login.php?incorrect_username");
         exit();
-        
+    } else {
+        $hashedPass = $user[0]->Password;
+        if (!password_verify($password, $hashedPass)) {
+            header("Location: ../View/login.php?incorrect_password");
+            exit();
+        } else {
+            $_SESSION['userId'] = $user[0]->User_ID;
+            $_SESSION['userUsername'] = $user[0]->Username;
+            $_SESSION['userEmail'] = $user[0]->Email_Address;
+            $_SESSION['userName'] = $user[0]->Name;
+            $_SESSION['profilePic'] = $user[0]->Profile_Picture;
+
+            header("Location: ../View/index.php");
+            exit();
+        }
     }
-    else
-    {
-        $error = true;
-        header("Location: ../View/login.php");
-        exit();
-    }
+    /* else
+      {
+      $error = true;
+      header("Location: ../View/login.php");
+      exit();
+      } */
 }
-/*else
-{
-    header("Location: ../View/login.php");
-    exit();
-}*/
+/* else
+  {
+  header("Location: ../View/login.php");
+  exit();
+  } */
 
 require_once '../View/login.php';
