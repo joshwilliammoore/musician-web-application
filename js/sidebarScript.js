1b4cc2d9d5003760d8c283a5470e6ea10a6c549e
Joshua Moore <K1720552@kingston.ac.uk>
2020-05-08 03:52:40 +0100

    JavaScript for sidebar.

---
+++ js/sidebarScript.js
@@ -0,0 +1,17 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+function openNav() {
+  document.getElementById("mySidenav").style.width = "400px";
+  document.getElementById("main").style.marginRight = "400px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginRight= "0";
+}
