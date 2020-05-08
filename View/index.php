817d8e7c17ad0b7e35c4a7dced4387ce6da68971
Joshua Moore <K1720552@kingston.ac.uk>
2020-05-08 03:44:08 +0100

    Index updated. Sidebar added and user types displayed.

--- View/index.php
+++ View/index.php
@@ -1,124 +1,131 @@
 <!DOCTYPE html>
-<?php include_once '../css/stylesheets.php';
+<?php
+require_once '../Controller/indexController.php';
+require_once '../Model/Users.php';
 ?>
 <html lang="en">
-    
-  <head>
-      
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/main.css">
-    <script src='https://kit.fontawesome.com/a076d05399.js'></script>
-    <title></title>
-    
+
+    <head>
+
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+        <?php include_once '../css/stylesheets.php'; ?>
+        <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+        <title></title>
+
     </head>
 
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class='container'>
-      <div class="navbar-header">
-  <a class="navbar-brand" href="/View/index.html"><img class="navbar-brand" href="/View/index.html" src="../images/Logo.png" alt="logo" style="width:40px">| Viewsician</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-      </div>
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <div class="nav-middle">
-      <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <input class="search_input" type="text" name="" placeholder="Search...">
-        <a href="#" class="search_icon"><i class="fas fa-search" style="font-size:25px"></i></a>
-      </li>
-      <li class="nav-item">
-        <a href="/View/search.html" class="icon"><i class="far fa-compass" style="font-size:30px" style="padding: 20px"></i></a>
-        <span class="text">Explore</span>
-      </li>
-      <li class="nav-item dropdown">
-        <!--<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">-->
-        <div class="dropdown">
-        <a href="/View/search.html" class="icon"><i class="far fa-user-circle" style="font-size:30px"></i></a>
-        <span class="text">Account</span>
-        <!--</a>-->
-          
-        <div class="dropdown-menu"><!--menu aria-labelledby="navbarDropdown"-->
-          <a class="dropdown-item" href="#">Action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Another action</a>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-        </div>
-      </li>
-      </div>
-      <li class="nav-item">
-        <a class="nav-link" href="/View/session.html">Session</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/View/artists.html">Artists</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/View/rehearsal.html">Rehearsal</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/View/venues.html">Venues</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/View/login.html">List</a>
-      </li>
-    </ul>
-  </div>
-          </div>
+    <?php include_once 'navbar.php';
+    include_once 'sidebar.php'; ?>
 
-</nav>
-  
-  <body>
-  <div class="container">
-  <div class="row">
-    <div class="col">
-      <h1>Account</h1>
-    </div>
-    <div class="col">
-      3 of 3
-    </div>
-    <div class="col">
-      3 of 3
-    </div>
-    <div class="col">
-      3 of 3
-    </div>
-  </div>
-  <div class="row">
-    <div class="col">
-      <a><img id="profile-pic" src="../images/profile-pic.png" alt="profile-pic" style="width:300px"></a>
-    </div>
-    <div class="col">
-      
-    </div>
-    <div class="col">
-      3 of 3
-    </div>
-    <div class="col">
-      3 of 3
-    </div>
-  </div>
-</div>  
-  <!--<div class="container">
-    <div class="row">
-        <div class="col-md-3">
-            <img src="" class="profile-pic" style="border-radius: 50%;">
+    <div id="main">
+        <div class="right">
+            <span style="font-size:20px;cursor:pointer" onclick="openNav()">&#9776; </span>
         </div>
-        <div class="col-md-9">
-            <h2 class="username"></h2>
-        </div>
-    </div>
-    </div>
-  </div>-->
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <body>
 
-  </body>
 
+            <div class="container">
+                <div class="col-md-10 images">
+                <div class="row">
+                    <div class="col">
+                        <h1>Index</h1>
+                    </div>
+                </div>
+
+             
+            <br><br>
+
+                <div class="row">
+                    <div class="col">
+                        <h2>Session Musicians</h2>
+                    </div>
+                </div>
+                <div id="gridview">
+<?php foreach ($sm as $details): ?>
+
+                        <div class="account">
+                            <img src="data:image/png;base64,<?= base64_encode($details->Profile_Picture) ?>" alt="profile-pic" id="pp" onerror="this.src='../images/profile-pic.png';"/>
+                            <h8><?= $details->Name ?></h8>
+                            <p><?= $details->Username ?></p>
+                            <form method="post" action="../View/profile.php">
+                                <a href="../View/profile.php?profile_id=<?= $details->User_ID ?>"><input class="buttons" id="abutton" type="button" name="view" value="View"></a>
+                            </form>
+
+                        </div>
+
+<?php endforeach ?>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <h2>Artists</h2>
+                    </div>
+                </div>
+                <div id="gridview">
+<?php foreach ($a as $details): ?>
+
+                        <div class="account">
+                            <img src="data:image/png;base64,<?= base64_encode($details->Profile_Picture) ?>" alt="profile-pic" id="pp" onerror="this.src='../images/profile-pic.png';"/>
+                            <h8><?= $details->Name ?></h8>
+                            <p><?= $details->Username ?></p>
+                            <form method="post" action="../View/profile.php">
+                                <a href="../View/profile.php?profile_id=<?= $details->User_ID ?>"><input class="buttons" id="abutton" type="button" name="view" value="View"></a>
+                            </form>
+
+                        </div>
+
+<?php endforeach ?>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <h2>Studios</h2>
+                    </div>
+                </div>
+                <div id="gridview">
+<?php foreach ($rs as $details): ?>
+
+                        <div class="account">
+                            <img src="data:image/png;base64,<?= base64_encode($details->Profile_Picture) ?>" alt="profile-pic" id="pp" onerror="this.src='../images/profile-pic.png';"/>
+                            <h8><?= $details->Name ?></h8>
+                            <p><?= $details->Username ?></p>
+                            <form method="post" action="../View/profile.php">
+                                <a href="../View/profile.php?profile_id=<?= $details->User_ID ?>"><input class="buttons" id="abutton" type="button" name="view" value="View"></a>
+                            </form>
+
+                        </div>
+
+<?php endforeach ?>
+                </div>
+                <div class="row">
+                    <div class="col">
+                        <h2>Venues</h2>
+                    </div>
+                </div>
+                <div id="gridview">
+<?php foreach ($v as $details): ?>
+
+                        <div class="account">
+                            <img src="data:image/png;base64,<?= base64_encode($details->Profile_Picture) ?>" alt="profile-pic" id="pp" onerror="this.src='../images/profile-pic.png';"/>
+                            <h8><?= $details->Name ?></h8>
+                            <p><?= $details->Username ?></p>
+                            <form method="post" action="../View/profile.php">
+                                <a href="../View/profile.php?profile_id=<?= $details->User_ID ?>"><input class="buttons" id="abutton" type="button" name="view" value="View"></a>
+                            </form>
+
+                        </div>
+
+<?php endforeach ?>
+                </div>
+
+                </div>
+            </div> 
+            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+            <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+        </body>
+    </div>
 </html>
 
