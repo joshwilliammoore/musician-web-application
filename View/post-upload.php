3ae6ff52b567e5a0b366e7bc32eadb3d8b084cfc
Joshua Moore <K1720552@kingston.ac.uk>
2020-05-08 03:45:48 +0100

    Post upload pages altered. Image enlarged within box.

--- View/post-upload.php
+++ View/post-upload.php
@@ -1,111 +1,83 @@
 <!doctype html>
-<?php include_once '../css/stylesheets.php'?>
+<?php
+require_once '../Controller/postUploadController.php';
+require_once '../Model/dataAccess.php';
+require_once '../Model/Posts.php';
+require_once '../Model/Users.php';
+?>
 <html lang="en">
-    
-  <head>
-      
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" href="../css/main.css">
-    <script src='https://kit.fontawesome.com/a076d05399.js'></script>
-    <title></title>
-    
+
+    <head>
+
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+
+        <?php include_once '../css/stylesheets.php'; ?>
+        <script src='https://kit.fontawesome.com/a076d05399.js'></script>
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <title></title>
+
     </head>
-    
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class='container'>
-      <div class="navbar-header">
-  <a class="navbar-brand" href="/View/index.html"><img class="navbar-brand" href="/View/index.html" src="../images/Logo.png" alt="logo" style="width:40px">| Viewsician</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-      </div>
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <div class="nav-middle">
-      <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <input class="search_input" type="text" name="" placeholder="Search...">
-        <a href="#" class="search_icon"><i class="fas fa-search" style="font-size:25px"></i></a>
-      </li>
-      <li class="nav-item">
-        <a href="/View/search.html" class="icon"><i class="far fa-compass" style="font-size:30px" style="padding: 20px"></i></a>
-        <span class="text">Explore</span>
-      </li>
-      <li class="nav-item dropdown">
-        <!--<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">-->
-        <div class="dropdown">
-        <a href="/View/search.html" class="icon"><i class="far fa-user-circle" style="font-size:30px"></i></a>
-        <span class="text">Account</span>
-        <!--</a>-->
-          
-        <div class="dropdown-menu"><!--menu aria-labelledby="navbarDropdown"-->
-          <a class="dropdown-item" href="#">Action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Another action</a>
-          <a class="dropdown-item" href="#">Something else here</a>
-        </div>
-        </div>
-      </li>
-      </div>
-      <li class="nav-item">
-        <a class="nav-link" href="/View/session.html">Session</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/View/artists.html">Artists</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/View/rehearsal.html">Rehearsal</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/View/venues.html">Venues</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="/View/login.html">List</a>
-      </li>
-    </ul>
-  </div>
-          </div>
 
-</nav>
-    
-  <body>
-      <br><br>
-  <div class="container">
-  <div class="row">
-    <div class="col">
-      <h4>Make a Post</h4>
-    </div>
-    <div class="col">
-      
-    </div>
-    <div class="col">
-      
-    </div>
-    <div class="col">
-      
-    </div>
-  </div>
-      <br><br>
-  <div class="row">
-    <div class="col-md-2">
-      
-    </div>
-    
-    <div class="col-md-7">
-        <div class="row mini-box"><textarea class="caption" rows='3'>Add Caption</textarea></div>
-        <div class="row mini-box post"><div class="post-box"><b class="plus">+</b></div></div>
-</div>
-    <div class="col-md-3 account-button">
-    </div>
-  </div>
+    <?php include_once 'navbar.php'; ?>
 
-</div>
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <body>
+        <br><br>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h4>Make a Post</h4>
+                </div>
+                
+            </div>
+            <br><br>
+            <div class="row">
+                <div class="col-md-2"></div>
+                <div class="col-md-8">
+                    <form method="post" enctype="multipart/form-data">
+                    
+                        <div class="row mini-box"><textarea name="caption" class="caption" rows='3' placeholder="Caption..."></textarea></div>
+                    <div class="row mini-box post">
+                        <div class="image-upload post-box">
+                            <img src="" id="profile-img-tag" width="500px" />
+                        </div>
+                    </div>
+                    </div>
+                <div class="col-md-2">
+                    <?php
+                        if (isset($message)) {
+                            echo '<label>' . $message . '</label>';
+                        }
+                        ?>
+                    <div class="buttons">
+                        <input type="file" name="photo" id="profile-img" 
+                               accept="image/gif, image/jpeg, image/png">
+                    </div>
+                    <button class="buttons" id="abutton" name="upload">Post</button>
+                    
+                    </form>
+                </div>
+
+            </div>
     </body>
+<script type="text/javascript">
+    function readURL(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+            
+            reader.onload = function (e) {
+                $('#profile-img-tag').attr('src', e.target.result);
+            }
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+    $("#profile-img").change(function(){
+        readURL(this);
+    });
+</script>
 </html>
 
 
